6d64683ba7b686c2eda209661fd6c3e9ea509f87
unknown <19252290@bsi.ac.id>
2026-06-02 16:51:58 +0700

    menambahkan logika nilai akhir

---
+++ nilai_akhir.php
@@ -0,0 +1,8 @@
+<?php
+// Menggunakan $nilai_akhir (dari Orang ke-3) dan $huruf (dari Orang ke-4)
+echo "<div class='result-card'>";
+echo "<h3>Hasil Penilaian:</h3>";
+echo "Total Nilai (Angka): <strong>" . $nilai_akhir . "</strong><br>";
+echo "Grade (Huruf): <span class='grade-highlight'>" . $huruf . "</span>";
+echo "</div>";
+?>
